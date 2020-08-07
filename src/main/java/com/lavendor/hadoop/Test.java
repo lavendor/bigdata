da96700142231c654bbefdb6944e4b6747b023f5
yanghao <1490508946@qq.com>
2020-08-07 16:05:08 +0800

    异常处理

--- src/main/java/com/lavendor/hadoop/Test.java
+++ src/main/java/com/lavendor/hadoop/Test.java
@@ -9,8 +9,24 @@ import java.util.Date;
 public class Test {
 
     public static void main(String[] args){
-        Test.dateFormat();
+
 //        Test.hashCodeTest();
+
+
+        try{
+            Test.dateFormat();
+            System.out.println("this is normal");
+            throw new Exception("exception");
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("this is exec");
+        }
+
+        System.out.println("after exception");
+        int a = 0;
+        a = 10;
+        System.out.println(a);
+
     }
 
     //测试hashCode值
@@ -22,24 +38,20 @@ public class Test {
     }
 
     //时间格式化
-    public static void dateFormat(){
+    public static void dateFormat() throws Exception{
         String line = "1952-12-02 14:21:01|34c";
         String dateStr = line.split("\\|")[0];
         System.out.println(dateStr);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Calendar cal = Calendar.getInstance();
-        try {
-            cal.setTime(sdf.parse(dateStr));
-            System.out.println(cal.get(Calendar.YEAR));
-            System.out.println(cal.get(Calendar.MONTH) + 1);
-            System.out.println(cal.get(Calendar.DAY_OF_MONTH));
-            System.out.println(cal.get(Calendar.HOUR_OF_DAY));
-            System.out.println(cal.get(Calendar.MINUTE));
-            System.out.println(cal.get(Calendar.SECOND));
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        cal.setTime(sdf.parse(dateStr));
+        System.out.println(cal.get(Calendar.YEAR));
+        System.out.println(cal.get(Calendar.MONTH) + 1);
+        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+        System.out.println(cal.get(Calendar.HOUR_OF_DAY));
+        System.out.println(cal.get(Calendar.MINUTE));
+        System.out.println(cal.get(Calendar.SECOND));
+        throw new Exception("this is dateFormat exception");
     }
 
 }
